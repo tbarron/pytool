f64f6e7f7ad8b09922fb778200c300a2f51eb24b
Tom Barron <tusculum@gmail.com>
2017-12-24 14:48:49 -0500

    Make pytool runnable, satisfying the first test

---
+++ pytool/__init__.py
@@ -0,0 +1,10 @@
+import sys
+
+def main():
+    """
+    """
+    print(sys.argv)
+    print("produce skeletons for python programs")
+
+if __name__ == "__main__":
+    main()
---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name="pytool",
+      author="Tom Barron",
+      author_email="tusculum@gmail.com",
+      entry_points={'console_scripts': ["pytool = pytool:main"]}
+      )
