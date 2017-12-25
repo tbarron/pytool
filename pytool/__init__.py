4fd8e19e469eac381f42ac02ee10a2e704c5ffc1
Tom Barron <tusculum@gmail.com>
2017-12-25 12:42:26 -0500

    Code that satisfies the tests for cfgdir()

--- pytool/__init__.py
+++ pytool/__init__.py
@@ -11,5 +11,24 @@ def main():
 
 
 # -----------------------------------------------------------------------------
+def cfgdir():
+    """
+    This function's task is to return the path of the configuration dir. If env
+    var PYTOOL_DIR is set, return its contents. Otherwise, if HOME is set,
+    return HOME/.pytool. If neither PYTOOL_DIR nor HOME is set, raise
+    FileNotFoundError
+    """
+    trydir = os.getenv("PYTOOL_DIR")
+    if trydir:
+        return(trydir)
+
+    homedir = os.getenv("HOME")
+    if homedir:
+        ptdir = os.path.join(homedir, ".pytool")
+        return(ptdir)
+
+    raise FileNotFoundError("Please set PYTOOL_DIR or HOME")
+
+
 if __name__ == "__main__":
     main()
