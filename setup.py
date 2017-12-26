328c0fbc34d681014e9ffeabdc21f56c94b4b517
Tom Barron <tusculum@gmail.com>
2017-12-26 11:05:02 -0500

    Use version in setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,8 @@
 from setuptools import setup
+from pytool import version
 
 setup(name="pytool",
+      version=version._v,
       author="Tom Barron",
       author_email="tusculum@gmail.com",
       entry_points={'console_scripts': ["pytool = pytool:main"]}
