db18991c0d25d528ecd40912d64ed865b444d94c
Tom Barron <tusculum@gmail.com>
2018-01-07 07:37:19 -0500

    Get version from pytool/version.py, use install_requires

--- setup.py
+++ setup.py
@@ -1,9 +1,24 @@
 from setuptools import setup
-from pytool import version
+
+def execfile(fname, gdict, ldict):
+    exec(compile(open(fname, 'rb').read(), fname, 'exec'),
+         gdict,
+         ldict)
+
+execfile("./pytool/version.py", globals(), locals())
 
 setup(name="pytool",
-      version=version._v,
+      version=__version__,
+      description="Create python projects and programs",
       author="Tom Barron",
       author_email="tusculum@gmail.com",
+      install_requires=[
+          'docopt_dispatch',
+          'py',
+          'pytest',
+          'GitPython',
+          'tbx',
+      ],
+      packages=['pytool'],
       entry_points={'console_scripts': ["pytool = pytool:main"]}
       )
